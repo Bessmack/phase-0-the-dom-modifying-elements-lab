143cafb6a0d6141fd762ed1a98de66fe5958d9f5
Bessmack <jenwied4lif@gmail.com>
2025-06-17 18:50:34 +0300

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Stephen-Njenga is the champion";
+document.body.appendChild(newHeader);
